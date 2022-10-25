1ce88879364345d1a444c36f590f79632cb4b459
AngelineLukmanova <angelinelukmanova@gmail.com>
2022-10-25 18:12:47 -0400

    Filter data, return aggregated results as JSON-encoded data

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,21 +1,30 @@
 import { Router, Request, Response } from 'express';
-import reposFromFile from '../../data/repos.json';
 import axios from 'axios';
+import reposFromFile from '../../data/repos.json';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
+  //add filtered repositories from JSON file to result
+  const result = reposFromFile.filter((repository) => !repository.fork);
+
   try {
     const reposFromAPI = await axios({
       url: 'https://api.github.com/users/silverorange/repos',
       method: 'get',
     });
+
+    //add filtered repositories from github API to result
+    result.push(
+      ...reposFromAPI.data.filter(
+        (repository: Record<string, any>) => !repository.fork
+      )
+    );
   } catch (err) {
     res.status(500).json({ message: err });
   }
-  res.status(200);
 
-  res.json([]);
+  res.status(200).json(result);
 });
