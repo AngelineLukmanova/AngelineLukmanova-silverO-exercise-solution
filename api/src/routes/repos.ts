e8e5586e95a7ba817bb4bf7ceea4c5474054b380
AngelineLukmanova <angelinelukmanova@gmail.com>
2022-10-25 18:03:14 -0400

    Import data from JSON file, fetch data from github API

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,21 @@
 import { Router, Request, Response } from 'express';
+import reposFromFile from '../../data/repos.json';
+import axios from 'axios';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
+  try {
+    const reposFromAPI = await axios({
+      url: 'https://api.github.com/users/silverorange/repos',
+      method: 'get',
+    });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
   res.status(200);
 
-  // TODO: See README.md Task (A). Return repo data here. You’ve got this!
   res.json([]);
 });
