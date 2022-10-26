668d07a09cd0a31c1bab05fd2bd90f1f22f7f4d4
AngelineLukmanova <angelinelukmanova@gmail.com>
2022-10-26 09:44:03 -0400

    Add onclick function to repos to fetch latest commit and readme file

--- web/src/components/repositories/Repositories.jsx
+++ web/src/components/repositories/Repositories.jsx
@@ -10,6 +10,7 @@ function Repositories() {
   const [data, setData] = useState();
   const [languageOptions, setLanguageOptions] = useState();
   const [displayedLanguage, setDisplayedLanguage] = useState('All');
+  const [openedRepo, setOpenedRepo] = useState(null);
 
   const compareDates = (date1, date2) => {
     return new Date(date2.created_at) - new Date(date1.created_at);
@@ -41,6 +42,44 @@ function Repositories() {
     }
   }, [data]);
 
+  const fetchLatestCommit = async (repo) => {
+    try {
+      const latestCommit = await axios.get(
+        `https://api.github.com/repos/${repo.owner.login}/${repo.name}/commits?per_page=1`
+      );
+      setOpenedRepo((info) => ({
+        ...info,
+        latestCommit: latestCommit.data?.[0],
+      }));
+    } catch (err) {
+      console.log(`Error! Error message: ${err.message}`);
+    }
+  };
+
+  const fetchReadmeFile = async (repo) => {
+    try {
+      const readmeFile = await axios.get(
+        `https://raw.githubusercontent.com/${repo.full_name}/master/README.md`
+      );
+      setOpenedRepo((info) => ({
+        ...info,
+        readmeFile: readmeFile.data,
+      }));
+    } catch (err) {
+      console.log(`Error! Error message: ${err.message}`);
+    }
+  };
+
+  const showFullInfo = (repo) => {
+    if (openedRepo?.id !== repo.id) {
+      setOpenedRepo({ id: repo.id });
+      fetchLatestCommit(repo);
+      fetchReadmeFile(repo);
+    } else {
+      setOpenedRepo(null);
+    }
+  };
+
   return (
     <div className="Repositories">
       <h1>Repositories</h1>
@@ -51,7 +90,11 @@ function Repositories() {
         />
       )}
       {data && (
-        <RepositoriesList data={data} displayedLanguage={displayedLanguage} />
+        <RepositoriesList
+          data={data}
+          displayedLanguage={displayedLanguage}
+          showFullInfo={showFullInfo}
+        />
       )}
     </div>
   );
--- web/src/components/repositories/RepositoriesList.jsx
+++ web/src/components/repositories/RepositoriesList.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
-function RepositoriesList({ data, displayedLanguage }) {
+function RepositoriesList({ data, displayedLanguage, showFullInfo }) {
   return (
     <div className="Repositories__list">
       {data?.map(
         (repo) =>
           (displayedLanguage === 'All' ||
             repo.language === displayedLanguage) && (
-            <div key={repo.id} className="Repositories__list-item">
+            <div
+              id={repo.id}
+              key={repo.id}
+              className="Repositories__list-item"
+              onClick={() => showFullInfo(repo)}
+            >
               <span>{`Name: ${repo.name}`}</span>
               <span>{`Description: ${repo.description}`}</span>
               <span>{`Language: ${repo.language}`}</span>
