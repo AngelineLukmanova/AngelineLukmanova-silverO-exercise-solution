966037cfc684afd8b4aa01743a5efc48a6c24d24
AngelineLukmanova <angelinelukmanova@gmail.com>
2022-10-25 20:01:58 -0400

    Add compareDates function to display data in reverse chronological order

--- web/src/components/repositories/Repositories.jsx
+++ web/src/components/repositories/Repositories.jsx
@@ -8,16 +8,20 @@ const expressAPI = process.env.REACT_APP_EXPRESS_API;
 function Repositories() {
   const [data, setData] = useState();
 
-  const getData = async () => {
-    try {
-      const response = await axios.get(`${expressAPI}/repos`);
-      setData(response?.data);
-    } catch (err) {
-      console.log(`Error! Error message: ${err.message}`);
-    }
+  const compareDates = (date1, date2) => {
+    return new Date(date2.created_at) - new Date(date1.created_at);
   };
 
   useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(`${expressAPI}/repos`);
+        setData(response.data.sort(compareDates));
+      } catch (err) {
+        console.log(`Error! Error message: ${err.message}`);
+      }
+    };
+
     if (!data) {
       getData();
     }
